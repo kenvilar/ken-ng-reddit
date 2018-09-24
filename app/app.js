06b5cd7c2226fe0a0458574fdced76a726372124
Ken Vilar <kenvilar@gmail.com>
2018-09-24 08:45:09 +0800

    Add post implementation

--- app/app.js
+++ app/app.js
@@ -6,12 +6,46 @@
 			controller: function() {
 				const self = this;
 
+				self.$onInit = () => {
+					self.posts = [];
+				};
+
 				self.tab = 1;
+
 				self.setTab = (selected) => {
 					self.tab = selected;
 				};
+
 				self.isSet = (tab) => {
 					return self.tab === tab;
+				};
+
+				self.addPost = () => {
+					self.posts.created_at = Date.now();
+					self.posts.votes = 0;
+					self.posts.commments = [];
+
+					self.posts.push({
+						title: self.posts.title,
+						body: self.posts.body,
+						author: self.posts.author,
+						img_url: self.posts.img,
+						created_at: self.posts.created_at,
+						votes: self.posts.votes,
+						comments: self.posts.commments
+					});
+
+					self.posts = {
+						title: '',
+						body: '',
+						author: '',
+						img_url: '',
+						created_at: '',
+						votes: '',
+						comments: ''
+					};
+
+					console.log(self.posts);
 				};
 			},
 			templateUrl: '/app/templates/reddit.html',
